fc16747ec69a1948e316bafb2df6e661c4706eb6
Manish-98 <manishlath3445@gmail.com>
2020-07-16 21:27:20 +0530

    [Add] Functionality to convert lists into movie objects

--- src/main/kotlin/com/github/Movie.kt
+++ src/main/kotlin/com/github/Movie.kt
@@ -3,6 +3,7 @@ package com.github
 import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
 import java.io.File
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 data class Movie(val imdbId: String, val title: String, val releaseDate: LocalDate, val genres: List<String>,
             val director: String, val actors: List<String>, val actresses: List<String>, val duration: Int){
@@ -32,6 +33,26 @@ class MovieStore() {
     var movies: List<Movie> = listOf(movieOne, movieTwo, movieThree, movieFour, movieFive)
 
     fun readCSVFile(fileName: String) : List<List<String>> = csvReader().readAll(File(fileName))
+
+    fun convertToObjectList(movieList: List<List<String>>) : List<Movie> {
+        val movies = mutableListOf<Movie>()
+        val formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd")
+
+        for (row in movieList.subList(1,movieList.size)) {
+            movies.add( Movie (
+                    title = row[0],
+                    releaseDate = LocalDate.parse(row[1], formatter),
+                    actors = row[2].split("|"),
+                    actresses = row[3].split("|"),
+                    genres = row[5].split("|"),
+                    imdbId = row[6],
+                    director = row[7],
+                    duration = Integer.parseInt(row[8])
+            ))
+        }
+
+        return movies
+    }
 
     fun movieReleasedInYear(year: Int) : List<Movie> = movies.filter {
             it.releaseDate.year == year
--- src/test/kotlin/com/github/MovieStoreTest.kt
+++ src/test/kotlin/com/github/MovieStoreTest.kt
@@ -58,4 +58,22 @@ class MovieStoreTest {
 
         movieStore.readCSVFile("/Users/manish/IdeaProjects/kotlin-hello-world/src/main/resources/MovieTest.csv") shouldBe expectedList
     }
+
+    @Test
+    fun `test conversion of lists into movie objects`() {
+        val movieStore = MovieStore()
+        val listOfLists = listOf(listOf("title","releaseDate","actors","actresses","collectionInCrore","genres","imdbID","director","duration"),
+                listOf("3 Idiots","2009/12/25","Amir Khan|Sanjay Dutt","Kareena Kapoor","326","Comedy|Drama","tt1187043","Raju Hirani","170"))
+        val expectedList = listOf(
+                Movie(title = "3 Idiots",
+                    releaseDate = LocalDate.of(2009, 12, 25),
+                    actors = listOf("Amir Khan", "Sanjay Dutt"),
+                    actresses = listOf("Kareena Kapoor"),
+                    genres = listOf("Comedy", "Drama"),
+                    imdbId = "tt1187043",
+                    director = "Raju Hirani",
+                    duration = 170))
+
+        movieStore.convertToObjectList(listOfLists) shouldBe expectedList
+    }
 }
